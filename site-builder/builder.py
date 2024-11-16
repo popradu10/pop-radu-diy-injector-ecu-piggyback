d38c812177704ad2458f190293e442bbd7ed8211
popradu10 <popradu10@popradu10.com>
2024-11-16 23:24:28 +0200

    enhance site builder with a template

--- site-builder/builder.py
+++ site-builder/builder.py
@@ -3,9 +3,15 @@
 
 import markdown
 
-# Read the Markdown content from a file
 input_file_path = "README.md"  # Path to your Markdown file
 output_file_path = "index.html"  # Path for the output HTML file
+
+placeholder = 'PLACEHOLDER_CONTENT'  # Placeholder to replace
+template_file_path = "site-builder/template.html"  # Path for the template HTML file
+
+# Open the template HTML file
+with open(template_file_path, 'r', encoding='utf-8') as file:
+    template_content = file.read()
 
 # Read Markdown content from the file
 with open(input_file_path, "r", encoding="utf-8") as file:
@@ -14,6 +20,9 @@ with open(input_file_path, "r", encoding="utf-8") as file:
 # Convert Markdown to HTML
 html_content = markdown.markdown(markdown_content)
 
+# Replace the placeholder with the new content
+final_html_content = template_content.replace(placeholder,html_content)
+
 # Save the HTML content to a new file
 with open(output_file_path, "w", encoding="utf-8") as file:
-    file.write(html_content)
+    file.write(final_html_content)
