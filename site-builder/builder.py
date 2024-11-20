34d06bc7930d17dd08a3d52b9898a2b721c83630
Pop Radu <pop.radu10@yahoo.com>
2024-11-21 00:01:32 +0200

    add presentation

--- site-builder/builder.py
+++ site-builder/builder.py
@@ -8,8 +8,10 @@ output_file_path = "index.html"  # Path for the output HTML file
 
 home_placeholder = 'HOME_PLACEHOLDER_CONTENT'  # Placeholder to replace
 video_placeholder = 'VIDEO_PLACEHOLDER_CONTENT'  # Placeholder to replace
+presentation_placeholder = 'PRESENTATION_PLACEHOLDER_CONTENT'  # Placeholder to replace
 template_file_path = "site-builder/template.html"  # Path for the template HTML file
 video_file_input_path = "site-builder/video.html"  # Path for the video HTML file
+presentation_file_input_path = "site-builder/presentation.html"
 
 # Open the template HTML file
 with open(template_file_path, 'r', encoding='utf-8') as file:
@@ -19,6 +21,10 @@ with open(template_file_path, 'r', encoding='utf-8') as file:
 with open(video_file_input_path, 'r', encoding='utf-8') as file:
     video_content = file.read()
 
+# Open the video HTML file
+with open(presentation_file_input_path, 'r', encoding='utf-8') as file:
+    presentation_content = file.read()
+
 # Read Markdown content from the file
 with open(input_file_path, "r", encoding="utf-8") as file:
     markdown_content = file.read()
@@ -27,7 +33,9 @@ with open(input_file_path, "r", encoding="utf-8") as file:
 html_content = markdown.markdown(markdown_content)
 
 # Replace the placeholder with the new content
-final_html_content = template_content.replace(home_placeholder, html_content).replace(video_placeholder, video_content)
+final_html_content = template_content.replace(home_placeholder, html_content).replace(video_placeholder,
+                                                                                      video_content).replace(
+    presentation_placeholder, presentation_content)
 
 # Save the HTML content to a new file
 with open(output_file_path, "w", encoding="utf-8") as file:
