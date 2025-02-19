e413ee329d9cfeeed4821d2d02a59eae5fb4c602
popradu10 <pop.radu10@yahoo.com>
2025-02-19 16:26:07 +0200

    Update diy-injector-ecu-piggyback.ino

--- diy-injector-ecu-piggyback/diy-injector-ecu-piggyback.ino
+++ diy-injector-ecu-piggyback/diy-injector-ecu-piggyback.ino
@@ -2,11 +2,11 @@
 
 int PERCENTAGE_LEVEL = 1;
 // Percentage of additional fuel injection at low RPM
-const int LOW_RPM_PERCENTAGE_LEVEL = 5;
+const int LOW_RPM_PERCENTAGE_LEVEL = 2;
 // Percentage of additional fuel injection at middle RPM
-const int MIDDLE_RPM_PERCENTAGE_LEVEL = 12;
+const int MIDDLE_RPM_PERCENTAGE_LEVEL = 5;
 // Percentage of additional fuel injection at high RPM
-const int HIGH_RPM_PERCENTAGE_LEVEL = 15;
+const int HIGH_RPM_PERCENTAGE_LEVEL = 10;
 
 // Threshold for low RPM range (RPM value below which LOW_RPM_PERCENTAGE_LEVEL is used)
 const int LOW_RPM_THRESHOLD = 3000;     //RPMs
